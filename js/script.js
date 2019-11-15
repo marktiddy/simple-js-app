bc7b267c8700b237bdf2b1be0316f656be662bd0
Mark Tiddy <marktiddy@gmail.com>
2019-11-15 15:54:53 +0000

    Script file now contains respository array

--- js/script.js
+++ js/script.js
@@ -1,32 +1,13 @@
-
-function anotherAlert() {
-  alert('Here is another alert which is embedded within a function');
-}
-
-alert('Hello World!');
-
-var favouriteFood = 'Chocolate';
-document.write(favouriteFood);
-
-console.log('Here we go ${favouriteFood}');
-
-//Some of my testing code from checking variables etc in the console. Will delete later.
-/*
-var myName = 'Mark';
-document.write(myName);
-myName = 'Mark Tiddy';
-document.write(myName);
-
-var simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-var multiply = 2 * 20;
-document.write(multiply);
-
-var size = 100;
-var doubleSize = size * 2;
-var minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-console.log(minSize)
-console.log(doubleSize)
-*/
+//Array that will eventually store Pokémon
+var repository = [
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: "Detective Pikachu",
+    height: 3,
+    types: ['coffee break', 'corkscrew punch']
+  }
+];
