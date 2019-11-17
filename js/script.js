d4cac7f3db969fe05955b3561fc33e29e8650048
Mark Tiddy <marktiddy@gmail.com>
2019-11-17 16:49:32 +0000

    Updated layout and added loops

--- js/script.js
+++ js/script.js
@@ -11,3 +11,11 @@ var repository = [
     types: ['coffee break', 'corkscrew punch']
   }
 ];
+
+for (var i = 0; i <= repository.length; i++) {
+  if (repository[i].height > 5) {
+    document.write(`<p><strong>${repository[i].name}</strong> (height: ${repository[i].height}) - <i>WOW! That's a tall Pokémon</i><br></p>`);
+  } else {
+    document.write(`<p><strong>${repository[i].name}</strong> (height: ${repository[i].height})<br></p>`);
+  }
+}
